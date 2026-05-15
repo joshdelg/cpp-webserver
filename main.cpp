f238d359a6c963477111d0a0e6a498e75cc1757f
Joshua Delgadillo <josh.delg03@gmail.com>
2026-05-15 15:12:56 -0700

    Use fork to enable multiple connections

--- main.cpp
+++ main.cpp
@@ -7,11 +7,33 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-int main(int argc, char* argv[]) {
-    std::cout << "Initializing server..." << std::endl;
+void handle_connection(int new_socket_fd) {
+    char buffer[256];
+    int bytes_read = read(new_socket_fd, buffer, 255);
 
-    int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if(bytes_read < 0) {
+        std::cout << "Error reading from buffer" << std::endl;
+    }
+
+    std::cout << "Here is the message: " << buffer << std::endl;
+
+    bytes_read = write(new_socket_fd, "I got your message", 18);
+    if (bytes_read < 0) {
+        std::cout << "Error writing to socket :(" << std::endl;
+    }
+}
+
+int main(int argc, char* argv[]) {
+    
+    if (argc < 2) {
+        std::cout << "Usage: " << argv[0] << " <port>" << std::endl;
+        exit(1);
+    }
+    
     int port = atoi(argv[1]);
+    
+    std::cout << "Initializing server on port " << port << std::endl;
+    int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
 
     struct sockaddr_in server_addr;
     server_addr.sin_family = AF_INET;
@@ -28,25 +50,25 @@ int main(int argc, char* argv[]) {
     struct sockaddr_in client_addr;
     socklen_t client_len = sizeof(client_addr);
 
-    int new_socket_fd = accept(socket_fd, (struct sockaddr*) &client_addr, &client_len);
-    if(new_socket_fd < 0) {
-        std::cout << "Error on accept" << std::endl;
+    while(1) {
+        int new_socket_fd = accept(socket_fd, (struct sockaddr*) &client_addr, &client_len);
+        if(new_socket_fd < 0) {
+            std::cout << "Error on accept" << std::endl;
+        }
+
+        // Recall: File descriptors are duplicated, but open file table is shared
+        pid_t child_process = fork();
+
+        if(child_process == 0) {
+            // Child's code
+            close(socket_fd);
+            handle_connection(new_socket_fd);
+            exit(0);
+        } else {
+            close(new_socket_fd);
+        }
     }
 
-    char buffer[256];
-    int bytes_read = read(new_socket_fd, buffer, 255);
-    if(bytes_read < 0) {
-        std::cout << "Error reading from buffer" << std::endl;
-    }
-
-    std::cout << "Here is the message: " << buffer << std::endl;
-
-    bytes_read = write(new_socket_fd, "I got your message", 18);
-    if (bytes_read < 0) {
-        std::cout << "Error writing to socket :(" << std::endl;
-    }
-
-    close(new_socket_fd);
     close(socket_fd);
 
     return 0;
